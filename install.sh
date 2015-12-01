4c777d68686253c343cca27bff8e73aa6a4b8bd9
Roman Treutlein <roman.treutlein@gmail.com>
2015-12-01 14:10:16 +0800

    Updated Code Now uses version 0.1.0.1 of opencog-atomspace

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+stack build
+
 libname=$(stack query | awk 'NR==2' | sed 's/://g'| sed 's/ //g')
 libver=$(stack query | awk 'NR==4' | sed 's/version: //g' | sed "s/'//g" | sed "s/ //g")
 
 LIB=$(find . -name "*$libname*.so" | awk 'NR==1')
 
-cp $LIB /usr/local/lib/opencog/"$libname-$libver"
+cp $LIB /usr/local/lib/opencog/"lib$libname-$libver.so"
